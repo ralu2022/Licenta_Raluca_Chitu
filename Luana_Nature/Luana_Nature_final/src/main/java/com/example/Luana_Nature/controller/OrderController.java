e03e780faa0035fac6ddce96ac1dcaac1623db24
Raluca-Oana Chițu <raluca.chitu2022@gmail.com>
2025-02-07 16:06:11 +0200

    Update OrderController.java

--- Luana_Nature/Luana_Nature_final/src/main/java/com/example/Luana_Nature/controller/OrderController.java
+++ Luana_Nature/Luana_Nature_final/src/main/java/com/example/Luana_Nature/controller/OrderController.java
@@ -1,9 +1,15 @@
 package com.example.Luana_Nature.controller;
 
 import com.example.Luana_Nature.model.Order;
+import com.example.Luana_Nature.model.User;
+import com.example.Luana_Nature.repository.UserRepository;
+import com.example.Luana_Nature.service.EmailService;
 import com.example.Luana_Nature.service.OrderService;
+import com.example.Luana_Nature.service.UserService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +21,7 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
-
+    private final UserService userService;
 
     @PostMapping("/addorder")
     public String addOrder(@RequestParam String productName,
@@ -25,20 +31,20 @@ public class OrderController {
                            @RequestParam LocalDate orderDate,
                            @RequestParam String status) {
 
-    Order order = orderService.addOrder(productName, orderedQuantity, pricePerItem, totalPrice, orderDate, status);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userService.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Utilizatorul nu a fost găsit!");
+        }
+
+    orderService.addOrder(productName, orderedQuantity, pricePerItem, totalPrice, orderDate,
+            status, user.getUserId());
 
     return "redirect:/store";
     }
 }
-
-
-    /*
-    @PostMapping("/addorder")
-    public ResponseEntity<String> submitOrder(@RequestBody Order order) {
-        orderService.addOrder(order);
-        return ResponseEntity.ok("OK!");
-    }
-} Comandă unică...*/
 
 
 
